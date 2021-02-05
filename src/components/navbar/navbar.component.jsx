71c25e859670809dc9c178b2aa241ad07a6ab52e
Joshua-Petrey <Joshua-Petrey@users.noreply.github.com>
2021-02-04 21:41:44 -0500

    Create and styled Navbar an Home components.

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        
+    <nav className="navbar">
+        <h1>DevCaine's Blog</h1>
+        <span class='writing-emoji'>&#10002;</span>
+
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create">New Blog</a>
+        </div>
+    </nav>
+     );
+}
+ 
+export default Navbar;
