8db262ec68c63b7a4b44a7e62d0e4ff8706e8e6d
Joshua-Petrey <Joshua-Petrey@users.noreply.github.com>
2021-02-09 16:30:25 -0500

    Added routes and styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import Navbar from './components/navbar/navbar.component';
 import Home from './components/home/home.component';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import CreateBlog from './components/create-blog/create-blog.component';
+import BlogDetails from './components/blog-details/blog-details.component';
 
 const App = () => {
   
   return (
+  <BrowserRouter>
     <div className="App">
       <Navbar />
       <div className='content'>
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home/>
+          </Route>
+          <Route path="/create">
+            <CreateBlog />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetails></BlogDetails>
+          </Route>
+        </Switch>
       </div>
     </div>
+  </BrowserRouter>
   );
 }
 // export components to use in other files
